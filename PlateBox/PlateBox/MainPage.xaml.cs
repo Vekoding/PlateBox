d2d14a14ec8272d3126fd692e2d733b3d6073b21
DESKTOP-TG4A12V\User_6 <mirjanafodor32@gmail.com>
2022-03-26 12:44:03 +0100

    Add project files.

---
+++ PlateBox/PlateBox/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Prism;
+using PlateBox.View;
+
+namespace PlateBox
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            
+            InitializeComponent();
+            Task.Run(async () =>
+            {
+                this.BackgroundColor = Color.Black;
+
+                await Task.Delay(3000);
+
+                this.BackgroundColor = Color.White;
+
+            }).Wait();
+
+
+        }
+
+
+        private  void Button_Clicked(object sender, EventArgs e)
+        {
+            var mergetDictionaries =
+                Application.Current.Resources.MergedDictionaries;
+            mergetDictionaries.Clear();
+            
+        }
+
+        private void Breakfast_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Breakfast());
+
+        }
+        private void Lunch_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Lunch());
+        }
+
+        private void Snack_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Snack());
+        }
+
+        private void Dinner_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Dinner());
+        }
+    }
+}
---
+++ PlateBox/PlateBox/Models/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace PlateBox.Models
+{
+    public class Ingredient
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("unit")]
+        public string Unit { get; set; }
+
+        [JsonProperty("amount")]
+        public string Amount { get; set; }
+    }
+
+
+
+}
